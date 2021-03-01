e6adb4a6b28483257715836d7a35dcdda40d6702
George5555ish <mbajiakugeorge@yahoo.com>
2021-03-01 22:34:56 +0100

    first commit

---
+++ src/main/java/com/example/oceangrsmithcodingassessment/OceangrsmithCodingAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.oceangrsmithcodingassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceangrsmithCodingAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceangrsmithCodingAssessmentApplication.class, args);
+	}
+
+}
