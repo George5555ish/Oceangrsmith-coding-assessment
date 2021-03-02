04ed25df1d67b26d494d803ad466e8c4431919ac
George5555ish <mbajiakugeorge@yahoo.com>
2021-03-02 02:44:13 +0100

    stored static locker variables to serve up lockers

--- src/main/java/com/example/oceangrsmithcodingassessment/locker/Locker.java
+++ src/main/java/com/example/oceangrsmithcodingassessment/locker/Locker.java
@@ -6,5 +6,49 @@ public class Locker {
 
     private Long id;
     private String location;
-    private List<String> availableLockers;
+    private String[] availableLockers;
+
+    public Locker() {
+    }
+
+    public Locker(Long id,
+                  String location,
+                  String[] availableLockers) {
+        this.id = id;
+        this.location = location;
+        this.availableLockers = availableLockers;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public  String[] getAvailableLockers() {
+        return availableLockers;
+    }
+
+    public void setAvailableLockers( String[] availableLockers) {
+        this.availableLockers = availableLockers;
+    }
+
+    @Override
+    public String toString() {
+        return "Locker{" +
+                "id=" + id +
+                ", location='" + location + '\'' +
+                ", availableLockers=" + availableLockers +
+                '}';
+    }
 }
